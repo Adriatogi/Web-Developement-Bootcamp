8b76461ef407e8fb22cd7890992aa90a9dc95be8
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2017-12-09 22:55:04 -0800

    Images now have equal height and added a horizontal line

--- My Projects/Small Javascript Quiz Game/js/questions.js
+++ My Projects/Small Javascript Quiz Game/js/questions.js
@@ -23,137 +23,110 @@ function character(total) {
   switch (total) {
     case 111:
       $("h2").text("You're character is Rick Sanchez");
-      restartButtons();
       $('#picture').prepend('<img id="rick" src=pictures/Rick.png />')
       break;
     case 211:
       $("h2").text("You're character is Morty Smith");
-      restartButtons();
       $('#picture').prepend('<img id="morty" src=pictures/Morty.png />')
       break;
     case 311:
       $("h2").text("You're character is Mr. Poopybutthole");
-      restartButtons();
       $('#picture').prepend('<img id="poopy" src=pictures/Poopy.png />')
       break;
     case 121:
       $("h2").text("You're character is the Cromulons");
-      restartButtons();
       $('#picture').prepend('<img id="cromulons" src=pictures/Cromulons.png />')
       break;
     case 221:
       $("h2").text("You're character is Summer Smith");
-      restartButtons();
       $('#picture').prepend('<img id="summer" src=pictures/Summer.png />')
       break;
     case 321:
       $("h2").text("You're character is Jerry Smith");
-      restartButtons();
       $('#picture').prepend('<img id="jerry" src=pictures/Jerry.png />')
       break;
     case 131:
       $("h2").text("You're character is Ma-Sha");
-      restartButtons();
       $('#picture').prepend('<img id="maSha" src=pictures/MaSha.png />')
       break;
     case 231:
       $("h2").text("You're character is Beth Smith");
-      restartButtons();
       $('#picture').prepend('<img id="beth" src=pictures/Beth.png />')
       break;
     case 331:
       $("h2").text("You're character is Jaguar");
-      restartButtons();
       $('#picture').prepend('<img id="jaguar" src=pictures/Jaguar.png />')
       break;
     case 112:
       $("h2").text("You're character is Birdperson");
-      restartButtons();
       $('#picture').prepend('<img id="bird" src=pictures/Bird.png />')
       break;
     case 212:
       $("h2").text("You're character is Alien Parasites");
-      restartButtons();
       $('#picture').prepend('<img id="parasites" src=pictures/Parasites.png />')
       break;
     case 312:
       $("h2").text("You're character is Hemorrhage");
-      restartButtons();
       $('#picture').prepend('<img id="hemorrhage" src=pictures/Hemorrhage.png />')
       break;
     case 122:
       $("h2").text("You're character is Snuffles");
-      restartButtons();
       $('#picture').prepend('<img id="snuffles" src=pictures/Snuffles.png />')
       break;
     case 222:
       $("h2").text("You're character is King Flippy Nips");
-      restartButtons();
       $('#picture').prepend('<img id="flippy" src=pictures/Flippy.png />')
       break;
     case 322:
       $("h2").text("You're character is Mr. Goldenfold");
-      restartButtons();
       $('#picture').prepend('<img id="golden" src=pictures/Golden.png />')
       break;
     case 132:
       $("h2").text("You're character is Unity");
-      restartButtons();
       $('#picture').prepend('<img id="unity" src=pictures/Unity.png />')
       break;
     case 232:
       $("h2").text("You're character is Krombopulos Michael");
-      restartButtons();
       $('#picture').prepend('<img id="krom" src=pictures/Krom.png />')
       break;
     case 332:
       $("h2").text("You're character is Morty Smith Jr.");
-      restartButtons();
       $('#picture').prepend('<img id="jr" src=pictures/Jr.png />')
       break;
     case 113:
       $("h2").text("You're character is Squanchy");
-      restartButtons();
       $('#picture').prepend('<img id="squanch" src=pictures/Squanch.png />')
       break;
     case 213:
       $("h2").text("You're character is Mr. Meeseeks");
-      restartButtons();
       $('#picture').prepend('<img id="meeseeks" src=pictures/meeseeks.png />')
       break;
     case 313:
       $("h2").text("You're character is Noob-Noob");
-      restartButtons();
       $('#picture').prepend('<img id="noob" src=pictures/Noob.png />')
       break;
     case 123:
       $("h2").text("You're character is Dr. Xenon Bloom");
-      restartButtons();
       $('#picture').prepend('<img id="doctor" src=pictures/Doctor.png />')
       break;
     case 223:
       $("h2").text("You're character is Doofus Rick");
-      restartButtons();
       $('#picture').prepend('<img id="doofus" src=pictures/Doofus.png />')
       break;
     case 323:
       $("h2").text("You're character is Ants in my Eyes Johnson");
-      restartButtons();
       $('#picture').prepend('<img id="johnson" src=pictures/Johnson.png />')
       break;
     case 133:
       $("h2").text("You're character is Evil Morty");
-      restartButtons();
       $('#picture').prepend('<img id="evil" src=pictures/Evil.png />')
       break;
     case 233:
       $("h2").text("You're character is Revolio Clockberg, Jr.");
-      restartButtons();
       $('#picture').prepend('<img id="gear" src=pictures/Gear.png />')
       break;
     case 333:
       $("h2").text("You're character is Abradolf Lincler");
-      restartButtons();
       $('#picture').prepend('<img id="abradolf" src=pictures/Abradolf.png />')
       break;
   }
@@ -180,6 +153,7 @@ function changeQuestion(){
   }
   else if(current ===3){
     character(total);
+    restartButtons();
   }
   else if(current ===4){  
     restart();
