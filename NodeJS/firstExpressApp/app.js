b512a8e216f39dea2f72397ad4cfa52eb35879d1
Adrian Gamarra <adriatogi@gmail.com>
2017-12-20 05:36:30 +0000

    Route params

--- NodeJS/firstExpressApp/app.js
+++ NodeJS/firstExpressApp/app.js
@@ -4,6 +4,13 @@ var app = express();
 //===============
 //ROUTES
 //===============
+
+app.get("/r/:subRedditName", function(req, res){
+    var subreddit = req.params.subRedditName.toUpperCase();
+    
+    res.send("Welcome to the "+subreddit+" subreddit!");
+});
+
 app.get("/", function(req, res) {
     res.send("Hi there!");
 });
@@ -14,6 +21,10 @@ app.get("/bye", function(req, res){
 
 app.get("/dog", function(req, res){
     res.send("Meow!");
+});
+
+app.get("*", function(req, res){
+    res.send("You're a star!!!");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
