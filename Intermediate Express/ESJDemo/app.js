e55c38ee3d26b4da2d4e2312671de5c0d8f9ac45
Adrian Gamarra <adriatogi@gmail.com>
2017-12-22 03:45:08 +0000

    EJS: Conditionals and Loops

--- Intermediate Express/ESJDemo/app.js
+++ Intermediate Express/ESJDemo/app.js
@@ -10,6 +10,16 @@ app.get("/:like/:thing", function(req, res){
     res.render('like.ejs', {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "Post 2", author: "Mike"},
+        {title: "Post 3", author: "Neat"}
+    ];
+    
+    res.render("posts.ejs", {posts: posts})
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening");
 });
