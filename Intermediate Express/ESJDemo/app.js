3ab90191a6d4fb94f8195a375e3e47161eb59d3e
Adrian Gamarra <adriatogi@gmail.com>
2017-12-22 04:28:29 +0000

    Serving Custom Acess

--- Intermediate Express/ESJDemo/app.js
+++ Intermediate Express/ESJDemo/app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/:like/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render('like.ejs', {thingVar: thing});
+    res.render('like', {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -17,7 +21,7 @@ app.get("/posts", function(req, res){
         {title: "Post 3", author: "Neat"}
     ];
     
-    res.render("posts.ejs", {posts: posts})
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
