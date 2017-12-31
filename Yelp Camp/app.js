ab42f7c03d25b692329551e893e6833be65726d2
Adrian Gamarra <adriatogi@gmail.com>
2017-12-31 23:16:09 +0000

    Campground Show Page

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 //Schema Setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("campgrounds", campgroundSchema); //arguement has to be plural for mongoose
@@ -19,6 +20,7 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+// Index - show all campgrounds
 app.get("/campgrounds", function(req, res){
     //Get campgrounds from database
     Campground.find({}, function(err, allCampgrounds){
@@ -26,15 +28,18 @@ app.get("/campgrounds", function(req, res){
             console.log("An error has occured");
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+//Create - add new campground to database
 app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var name = req.body.name,
+    image = req.body.image,
+    desc = req.body.description,
+    newCampground = {name: name, image: image, description: desc};
+    
     //create a new campground and save to DB
     Campground.create(newCampground, function(err, newCampground){
          if(err){
@@ -47,8 +52,21 @@ app.post("/campgrounds", function(req, res){
     res.redirect("/campgrounds");
 });
 
+//New - show form to create a new campground
 app.get("/campgrounds/new", function(req,res){
     res.render("new.ejs");
+});
+
+//Show - show more information of one campground
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log("An error has occured");
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
