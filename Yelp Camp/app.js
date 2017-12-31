05557f856c7cbaa05a86e490dd8b81a8e9728f5a
Adrian Gamarra <adriatogi@gmail.com>
2017-12-31 06:12:10 +0000

    YelpCamp adding mongoose

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -1,33 +1,49 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
-var campgrounds=[
-        {name: "Salmon Creek", image: "https://www.photosforclass.com/download/5641024448"},
-        {name: "Granite Hill", image: "https://www.photosforclass.com/download/4369518024"},
-        {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/2602356334"},
-        {name: "Salmon Creek", image: "https://www.photosforclass.com/download/5641024448"},
-        {name: "Granite Hill", image: "https://www.photosforclass.com/download/4369518024"},
-        {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/2602356334"},
-        {name: "Granite Hill", image: "https://www.photosforclass.com/download/4369518024"},
-        {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/2602356334"}
-];
+var express = require('express'), // if it is consecutive variable decleration, you dont have to say var in the beggining and seperate each variable with a comma
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("campgrounds", campgroundSchema); //arguement has to be plural for mongoose
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //Get campgrounds from database
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log("An error has occured");
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+    //create a new campground and save to DB
+    Campground.create(newCampground, function(err, newCampground){
+         if(err){
+            console.log("An error has occured");
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
     res.redirect("/campgrounds");
 });
 
